673d0b83e601c2889ee2d0abc7b43526552d8249
mbashir1 <mbashir1@B70718-LAPTOP.ad.douglascollege.ca>
2023-11-30 05:32:58 -0800

    This commit is by the team member

--- GitProject/src/Practice/Hello.java
+++ GitProject/src/Practice/Hello.java
@@ -5,6 +5,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git demo project created by a team leader");
+		method();
+		
+	}
+	
+	public static void method() {
+		System.out.println("a method created by a team member");
 	}
 
 }
